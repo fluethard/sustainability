671f20223f6b28ce40c7742d01d57194673c572b
fluethard <fabian@luethard.com>
2023-02-07 11:09:35 +0100

    Create FL_sandbox.R

---
+++ sustainability_group/FL_sandbox.R
@@ -0,0 +1,32 @@
+library(tidyverse)      # select
+library(ggplot2)    # ggplot
+library(reshape2) 
+
+air_co <- air %>%
+  select(Datum, CO)
+
+plot(air_co)
+
+# trees by year
+tree_ts <- d.trees %>%
+  select(pflanzjahr, objid) %>%
+  filter(pflanzjahr > 1980) %>%
+  group_by(pflanzjahr) %>%
+  summarise(count = n())
+
+# Trees by district
+tree_quartier <- d.trees %>%
+  select(pflanzjahr, objid, quartier) %>%
+  filter(pflanzjahr > 1980) %>%
+  group_by(pflanzjahr, quartier) %>%
+  summarise(count = n())
+
+
+yearly_air <- air %>%
+  add_column(year = lubridate::year(air$Datum))
+
+
+yearly_co <- yearly_air %>%
+  select(year, CO) %>%
+  group_by(year) %>%
+  summarise(mean_co = mean(CO, na.rm = T))
